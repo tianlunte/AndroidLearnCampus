04688dae8f46e3ea9e20bd2a3152bdeea1b1c14b
qingyun.wang <wangqingyun123@yahoo.com>
2018-03-01 20:02:01 +0800

    added ItemInfoHolder

---
+++ recyclerview-analysis/src/main/java/com/wangqingyun/recyclerviewanalysis/view_info/ItemHolderInfo.java
@@ -0,0 +1,30 @@
+package com.wangqingyun.recyclerviewanalysis.view_info;
+
+/**
+ * Created by qingyun.wang on 28/02/2018.
+ */
+
+public class ItemHolderInfo {
+    /**
+     * The left edge of the View (excluding decorations)
+     */
+    public int left;
+
+    /**
+     * The top edge of the View (excluding decorations)
+     */
+    public int top;
+
+    /**
+     * The right edge of the View (excluding decorations)
+     */
+    public int right;
+
+    /**
+     * The bottom edge of the View (excluding decorations)
+     */
+    public int bottom;
+
+    public ItemHolderInfo() {
+    }
+}
